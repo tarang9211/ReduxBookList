9e37f47a448b6c53726e9e4ed3dc25650d418dfb
Tarang Hirani <tarang9211@gmail.com>
2016-11-11 01:30:17 +0530

    created books reducer and composed it in rootReducer object

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+  return [
+    { title: 'JavascriptL The good parts' },
+    { title: 'Harry Potter' },
+    { title: 'The Monk who sold his Ferrari ' },
+    { title: 'To Kill A Mockingbird' },
+    { title: '' },
+    { title: '' },
+  ]
+}
